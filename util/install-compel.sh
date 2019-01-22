d7c1274e78dcf380d362a74123e06b39816df3c3
Rob Lyerly <rlyerly@vt.edu>
2019-01-22 09:59:35 -0500

    Added compel patch to allow controlling multiple tracees

--- util/install-compel.sh
+++ util/install-compel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-CRIU=$(readlink -f .)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+COMPEL_PATCH=$(readlink -f $DIR/../patches/compel.patch)
 INSTALL=/usr/local/chameleon
 LIBS=$INSTALL/lib64/release
 ARGS="-j$(nproc)"
+PATCH=1
 DEBUG=0
 
 function print_help {
@@ -13,6 +15,7 @@ function print_help {
   echo "  -h | --help        : print help & exit"
   echo "  -c | --criu DIR    : CRIU source directory"
   echo "  -i | --install DIR : set installation directory"
+  echo "  -n | --no-patch    : don't apply Chameleon's patch to compel sources"
   echo "  -d | --debug       : build debug version of libcompel"
   echo
   echo "See https://criu.org/Main_Page for more information on CRIU"
@@ -28,6 +31,7 @@ while [[ $1 != "" ]]; do
     -h | --help) print_help; exit 0;;
     -c | --criu) CRIU=$(readlink -f $2); shift;;
     -i | --install) INSTALL=$(readlink -f $2); shift;;
+    -n | --no-patch) PATCH=0;;
     -d | --debug)
       DEBUG=1
       ARGS="$ARGS DEBUG=1"
@@ -36,14 +40,24 @@ while [[ $1 != "" ]]; do
   shift
 done
 
+if [[ -z "$CRIU" ]]; then
+  print_help
+  echo
+  die "CRIU source directory not set";
+fi
+cd $CRIU || die "directory '$CRIU' does not exist"
+if [[ ! -f "Makefile.compel" ]] || [[ ! -d "compel" ]]; then
+  die "'$CRIU' is not a valid CRIU source directory"
+fi
+
 echo -e "Installing from source directory:\n  $(readlink -f $CRIU)\n" \
         "to installation directory:\n  $(readlink -f $INSTALL)"
 if [[ $DEBUG -eq 1 ]]; then echo " -> DEBUG build <-"; fi
 echo
 
-cd $CRIU || die "directory '$CRIU' does not exist"
-if [[ ! -f "Makefile.compel" ]] || [[ ! -d "compel" ]]; then
-  die "'$CRIU' is not a valid CRIU source directory"
+if [[ $PATCH -eq 1 ]]; then
+  if [[ ! -f "$COMPEL_PATCH" ]]; then die "could not find compel patch"; fi
+  patch -p1 < $COMPEL_PATCH || die "could not patch compel"
 fi
 
 make mrproper || die "could not build compel"
